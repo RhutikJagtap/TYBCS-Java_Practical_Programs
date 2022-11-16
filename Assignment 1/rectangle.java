3f5408919796c6f37208d8dbc312e4454ade3c5e
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-16 10:53:51 +0530

    Add files via upload

---
+++ Assignment 1/rectangle.java
@@ -0,0 +1,17 @@
+//Set A-b)  programm to calculate and area of rectangle
+ import java.util.*;
+class rectangle {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length:");
+        int length = sc.nextInt();
+        System.out.println("Enter breadth:");
+        int bredth= sc.nextInt();
+        int area=length*bredth;
+        System.out.println("Area of rectangle:"+area);
+        int perimeter=2*(length+bredth);
+        System.out.println("Perimeter of rectangle:"+perimeter);
+
+    }
+}
