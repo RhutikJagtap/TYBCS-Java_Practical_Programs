a00655a8453a0d41ea048021aaccf7240b92917c
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-16 10:49:24 +0530

    Update Myclass.java

--- Assignment 1/Myclass.java
+++ Assignment 1/Myclass.java
@@ -1,1 +1,30 @@
+/*Set A-a)using javap,view the methods of the following classes from the lang package
+java.lang.object,java.lang.string and java.util.scanner. and also compile sample programm.
+ type the command and view the bytecodes. javap -c Myclass*/
+public class Myclass {
+    int num;
+    public Myclass()
+    {
+        num=0;
+    }
+    public Myclass(int num)
+    {
+        this.num=num;
+    }
+  public static void main(String[] args) 
+    //public static void main(java.lang.String[]) 
+   {
+    Myclass m1=new Myclass();
+    if(args.length>0)
+    {
+        int n=Integer.parseInt(args[0]);
+        Myclass m2=new Myclass(n);
+        System.out.println(m1.num);
+        System.out.println(m2.num);
+    }
+    else
+    System.out.println("Insufficient arguments");
+   }
+
+}
 
