e46c068d2b6b92f3a5b799294d570362e07537b1
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-16 19:40:04 +0530

    Update studentsort.java

--- Assignment 2/studentsort.java
+++ Assignment 2/studentsort.java
@@ -1,8 +1,6 @@
 /*Set A
 b.Define Student class(roll_no, name, percentage) to create n objects of the Student class. Accept details from the user for each object. Define a static method "sortStudent" which sorts the array on the basis of percentage.
 */
-
-
 import java.util.*;
 
 public class studentsort {
